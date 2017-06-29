fe00e1810858497d62f726de0416ac17e3d50ba3
clarkwaite <clarkwaite@gmail.com>
2017-06-29 15:55:55 -0400

    update posts error

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,3 @@
 class City < ApplicationRecord
 	has_many :posts, dependent: :destroy
-
-	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user, dependent: :destroy
-  belongs_to :city, optional:true, dependent: :destroy
+  belongs_to :user                   
+  belongs_to :city, optional:true
   validates :title, presence: true, length: { maximum: 200, minimum: 1 }
   validates :post, presence: true
   validates :img, presence: true
