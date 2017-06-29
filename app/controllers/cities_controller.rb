222943f3cf535c9b4a5a2e766973d4da31889489
WilliamRausch <nolanrausch@Gmail.com>
2017-06-29 12:34:38 -0400

    search bar resize

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,13 @@
 class CitiesController < ApplicationController
 
   def index
-    @cities = City.all
- 
+    if params[:name]
+    @city = City.where('store LIKE ?', "%#{params[:name]}%")
+  else
+    @city = City.all
+  end
+
+
   end
 
   def show
@@ -38,6 +43,7 @@ class CitiesController < ApplicationController
   private
   def city_params
     params.require(:city).permit(:name, :state, :image)
+
   end
 
 end
