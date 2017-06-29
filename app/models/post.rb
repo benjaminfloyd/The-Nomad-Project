f391228f391fb8392e78812746f837e3bc0e257c
clarkwaite <clarkwaite@gmail.com>
2017-06-29 14:47:54 -0400

    one more change to validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   belongs_to :city, optional:true, dependent: :destroy
   validates :title, presence: true, length: { maximum: 200, minimum: 1 }
   validates :post, presence: true
+  validates :img, presence: true
 end
