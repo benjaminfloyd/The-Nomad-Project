ea5e33d3efa3c6fd618b9bba4bc8ceaf1dde3de4
clarkwaite <clarkwaite@gmail.com>
2017-07-02 11:44:27 -0400

    Seeds are complete - 2 users, 4 cities, a lot of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user                   
   belongs_to :city, optional:true
-  validates :title, presence: true, length: { maximum: 200, minimum: 1 }
-  validates :post, presence: true
+  validates :title, presence: true, length: { maximum: 50, minimum: 1 }
+  validates :post, presence: true, length: { maximum: 500, minimum: 1 }
   validates :img, presence: true
 end
