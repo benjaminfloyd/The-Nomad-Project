6566d8f090bbda3a0a54ad602d1f3aad28d1c2d9
clarkwaite <clarkwaite@gmail.com>
2017-07-02 19:05:14 -0400

    fixed new user error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,16 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
 
+ protect_from_forgery with: :exception
+ # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+      def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :full_name, :current_city, :image) }
+          devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :full_name, :current_city, :image) }
+      end
+
 
   def home
     render 'cities/home'
